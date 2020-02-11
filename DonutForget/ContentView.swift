0310914e57ad40cf9719bb98c6b6fe84ecace7dd
Conrado Dembiski <conradodembiski@gmail.com>
2020-02-11 10:23:23 -0300

    Initial Commit

---
+++ DonutForget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DonutForget
+//
+//  Created by João Conrado Santana de Lima Dembiski on 11/02/20.
+//  Copyright © 2020 João Conrado Santana de Lima Dembiski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DonutForgetTests/DonutForgetTests.swift
@@ -0,0 +1,34 @@
+//
+//  DonutForgetTests.swift
+//  DonutForgetTests
+//
+//  Created by João Conrado Santana de Lima Dembiski on 11/02/20.
+//  Copyright © 2020 João Conrado Santana de Lima Dembiski. All rights reserved.
+//
+
+import XCTest
+@testable import DonutForget
+
+class DonutForgetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
